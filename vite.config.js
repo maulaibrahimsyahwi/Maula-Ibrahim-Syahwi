33f2e9081640611e6bbcd1beb9290f5dd2040e58
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-10 21:50:35 +0700

    fitur

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,16 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/Web-Portofolio-MaulaIbrahimSyahwi/",
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.[jt]sx?$/,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx", // Treat .js files as JSX
+  plugins: [
+    react({
+      // Experimental: handle JSX MIME types
+      babel: {
+        plugins: [],
       },
-    },
+    }),
+    tailwindcss(),
+  ],
+  define: {
+    "process.env": {},
   },
 });
