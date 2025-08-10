259c2383a2c960fc73481da4d0a02eab25b47a28
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-10 21:28:01 +0700

    Fitur

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+      loader: {
+        ".jsx": "jsx",
+        ".js": "jsx",
+      },
+    },
+  },
+  build: {
+    target: "es2020",
+  },
 });
