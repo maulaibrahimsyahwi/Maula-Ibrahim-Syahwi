ee040b7dcf75c1c8cd86cdf3a7af0897e020d551
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-11 05:16:09 +0700

    first

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: process.env.VITE_BASE_PATH || "/Portofolio-Maula-Ibrahim-Syahwi/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash][extname]",
+      },
+    },
+  },
 });
