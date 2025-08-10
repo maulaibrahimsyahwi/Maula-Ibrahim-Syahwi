9864f64e99a467085f265adc467b4db2896be0d9
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-10 22:28:28 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Experimental: handle JSX MIME types
-      babel: {
-        plugins: [],
-      },
-    }),
-    tailwindcss(),
-  ],
-  define: {
-    "process.env": {},
-  },
+  plugins: [react(), tailwindcss()],
+  base: "/Portofolio-Maula-Ibrahim-Syahwi/",
 });
