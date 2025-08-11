ec904453f45233dfb4d4be1f64e4d9a36c498580
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-08-11 07:59:33 +0700

    link

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Portofolio-Maula-Ibrahim-Syahwi/",
+  base: "/Maula-Ibrahim-Syahwi/",
   build: {
     rollupOptions: {
       output: {
