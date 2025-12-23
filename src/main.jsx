e7c2adb5b1476828e5213b90fcf8924a969f0ec3
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-12-23 10:48:06 +0700

    use lenis

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ReactLenis } from "lenis/react";
 import "./index.css";
 import App from "./App.jsx";
 import Navbar from "./pages/Navbar.jsx";
@@ -10,26 +11,23 @@ import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import MusicPlayer from "./components/Dock/MusicPlayer.jsx";
+import "lenis/dist/lenis.css";
 
-// Initialize AOS
 AOS.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PreLoader basename={import.meta.env.BASE_URL}>
-      {/* Container dengan relative positioning untuk MusicPlayer */}
-      <div className="relative">
-        {/* MusicPlayer positioned absolutely */}
-        <MusicPlayer />
-
-        {/* Main content */}
-        <div className="container mx-auto px-4 mb-10">
-          <Navbar />
-          <App />
+    <ReactLenis root>
+      <PreLoader basename={import.meta.env.BASE_URL}>
+        <div className="relative">
+          <MusicPlayer />
+          <div className="container mx-auto px-4 mb-10">
+            <Navbar />
+            <App />
+          </div>
         </div>
-      </div>
-
-      <Footer />
-    </PreLoader>
+        <Footer />
+      </PreLoader>
+    </ReactLenis>
   </StrictMode>
 );
